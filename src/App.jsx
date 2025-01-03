28b22a97968a6cf2fdcb8fb3bb710df0163050a0
Karina1014 <janneth_wi15@hotmail.com>
2025-01-03 14:46:06 -0500

    feat: :tada: Create project
    This project MERN

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import { Login } from './pages/Login';
+import { ResetPassword } from './pages/ResetPassword';
+import EmailVerify from './pages/EmailVerify';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/email-verify' element={<EmailVerify/>}></Route>
+        <Route path='/reset-password' element={<ResetPassword/>}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
