a28833f5d63d8410f638f0a221a89e00e7f1c84d
Karina1014 <janneth_wi15@hotmail.com>
2025-01-03 23:05:43 -0500

    feat: :white_check_mark: update
    fix username capitalization error

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,33 @@
-import {assets} from '../assets/assets'
-import { useNavigate } from 'react-router-dom'; //navegar Hook en enrutador
+import { useContext } from 'react';
+import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom'; // Navegar Hook en enrutador
+import { AppContent } from '../context/AppContext';
 
 const Navbar = () => {
-
   const navigate = useNavigate();
+  const { userData, backendUrl, setUserData, setIsLoggedin } = useContext(AppContent);
 
   return (
-    <div className='w-full flex justify-between items-center p-4 sm:p-6
-    sm:px-24 absolute top-0'>
-        <img src={assets.logo} alt="logo"  className='w-28 sm:w-32'/>
-        <button onClick={()=>navigate('/login')}
-        className='flex items-center gap-2 border border-gray-500 rounder-full
-        px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Login<img src={assets.arrow_icon} alt="Flecha"/></button>
+    <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
+      <img src={assets.logo} alt="logo" className='w-28 sm:w-32' />
+      {userData ? (
+        <div className='w-8 h-8 flex justify-center items-center rounded-full
+        bg-black text-white relative group'>
+          {/* Corregido el error con toUpperCase */}
+          {userData.name.charAt(0).toUpperCase() }
+          {/* {userData.name.charAt(0).toUpperCase() + userData.name.slice(1)} */}
+        </div>
+      ) : (
+        <button
+          onClick={() => navigate('/login')}
+          className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'
+        >
+          Login
+          <img src={assets.arrow_icon} alt="Flecha" />
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
