967c9afb3a7d1093be4e75e3299e9db713c2f50f
Jesdhy <pinchajessy529@gmail.com>
2025-01-23 12:37:15 -0500

    feat: :tada: add new branch
    add new files for branch main

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import exprees from 'express'
-import userAuth from '../middleware/userAuth.js';
-import { getUserData } from '../controllers/userController.js';
-
-const userRoutes = exprees.Router();
-
-userRoutes.get('/data', userAuth, getUserData)
-
-export default userRoutes;
